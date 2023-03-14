49d19d64e9a71fc22723fadcdf661e6ee775903a
Sabya <sabya7011@gmail.com>
2023-03-15 00:46:34 +0530

    Global variables

---
+++ GlobalVariable.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract GlobalVariable {
+
+    address public myadd = msg.sender;
+    uint public time = block.timestamp;
+    uint public diff = block.difficulty;
+    uint public gascost = tx.gasprice;
+}
