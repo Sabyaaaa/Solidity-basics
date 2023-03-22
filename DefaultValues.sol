49f8ae501e016590b10f59a9e1ac86f5426082b1
Sabya <sabya7011@gmail.com>
2023-03-22 12:48:25 +0530

    Default values of datatypes

---
+++ DefaultValues.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract DefaultValue {
+
+    uint public x;
+    bool public y;
+    address public z;
+    string public str;
+    bytes32 public a;
+}
